c4ada2d594ec843adf7784f1ffe9b12f816d96de
Icezypanu <68285395+Icezypanu@users.noreply.github.com>
2020-08-02 02:14:26 +0700

    Create Exercise4_Panupong_K.py

---
+++ Exercise4_Panupong_K.py
@@ -0,0 +1,9 @@
+FunEng = 20
+GenBu = 19
+IntroCom = 30
+ComPro = 40
+
+print('Foundation English :',FunEng)
+print('General Business :',GenBu)
+print('Introduction to Computer Systems :',IntroCom)
+print('Computer Programming :',ComPro)
